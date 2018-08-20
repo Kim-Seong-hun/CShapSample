e580c00a202b492d66e1562e165d59ef3485e804
KiM-Seong-hun <darkhok@naver.com>
2018-08-20 15:34:07 +0900

    Encryption First Commit

---
+++ Encryption/Encryption/AES.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Encryption
+{
+    public static class AES
+    {
+        private static Encryption _encryption = new Encryption();
+
+
+        public static int GetKeySize()
+        {
+            return _encryption.KeySize;
+        }
+
+
+        public static void ChangeKeySize(int keySize)
+        {
+            if (keySize <= 128)
+                _encryption.SettingKeySize128();
+            else
+                _encryption.SettingKeySize256();
+        }
+
+        public static String EncryptStringToString(String input, String key)
+        {
+            return _encryption.EncryptStringToString(input, key);
+        }
+
+        public static String EncryptByteToString(Byte[] input, String key)
+        {
+            return _encryption.EncryptByteToString(input, key);
+        }
+
+        public static Byte[] EncryptStringToByte(String input, String key)
+        {
+            return _encryption.EncryptStringToByte(input, key);
+        }
+
+        public static Byte[] EncryptByteToByte(Byte[] input, String key)
+        {
+            return _encryption.EncryptByteToByte(input, key);
+        }
+
+
+        public static String DecryptStringToString(String input, String key)
+        {
+            return _encryption.DecryptStringToString(input, key);
+        }
+
+        public static String DecryptByteToString(Byte[] input, String key)
+        {
+            return _encryption.DecryptByteToString(input, key);
+        }
+
+        public static Byte[] DecryptStringToByte(String input, String key)
+        {
+            return _encryption.DecryptStringToByte(input, key);
+        }
+
+        public static Byte[] DecryptByteToByte(Byte[] input, String key)
+        {
+            return _encryption.DecryptByteToByte(input, key);
+        }
+    }
+}
---
+++ Encryption/Encryption/Encryption.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryption
+{
+    class Encryption
+    {
+        private int _keySize;
+        public int KeySize { get => this._keySize; }
+
+
+        public Encryption()
+        {
+            this._keySize = 256;
+        }
+
+
+        public void SettingKeySize128()
+        {
+            this._keySize = 128;
+        }
+
+        public void SettingKeySize256()
+        {
+            this._keySize = 256;
+        }
+
+        public String EncryptStringToString(String input, String key)
+        {
+            Byte[] result = ProcessingEncryption(Encoding.UTF8.GetBytes(input), key, "encrypt");
+            String output = Convert.ToBase64String(result);
+
+            return output;
+        }
+
+
+        public String EncryptByteToString(Byte[] input, String key)
+        {
+            Byte[] result = ProcessingEncryption(input, key, "encrypt");
+            String output = Convert.ToBase64String(result);
+
+            return output;
+        }
+
+
+        public Byte[] EncryptStringToByte(String input, String key)
+        {
+            return ProcessingEncryption(Encoding.UTF8.GetBytes(input), key, "encrypt");
+        }
+
+
+        public Byte[] EncryptByteToByte(Byte[] input, String key)
+        {
+            return ProcessingEncryption(input, key, "encrypt");
+        }
+
+
+        public String DecryptStringToString(String input, String key)
+        {
+            Byte[] result = ProcessingEncryption(Convert.FromBase64String(input), key, "decrypt");
+            String output = Encoding.UTF8.GetString(result);
+
+            return output;
+        }
+
+
+        public String DecryptByteToString(Byte[] input, String key)
+        {
+            Byte[] result = ProcessingEncryption(input, key, "decrypt");
+            String output = Encoding.UTF8.GetString(result);
+
+            return output;
+        }
+
+
+        public Byte[] DecryptStringToByte(String input, String key)
+        {
+            return ProcessingEncryption(Convert.FromBase64String(input), key, "decrypt");
+        }
+
+
+        public Byte[] DecryptByteToByte(Byte[] input, String key)
+        {
+            return ProcessingEncryption(input, key, "decrypt");
+        }
+
+
+        private byte[] ProcessingEncryption(Byte[] input, String key, String type)
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+
+            rijndaelCipher.Mode = CipherMode.CBC;
+            rijndaelCipher.Padding = PaddingMode.PKCS7;
+            rijndaelCipher.KeySize = 256;
+            rijndaelCipher.BlockSize = 128;
+
+            byte[] pwdBytes = Encoding.UTF8.GetBytes(key);
+            byte[] keyBytes = new byte[16];
+            int len = pwdBytes.Length;
+
+            if (len > keyBytes.Length)
+                len = keyBytes.Length;
+
+            Array.Copy(pwdBytes, keyBytes, len);
+
+            rijndaelCipher.Key = keyBytes;
+            rijndaelCipher.IV = keyBytes;
+
+            if (type.ToLower().Equals("encrypt"))
+            {
+                ICryptoTransform transform = rijndaelCipher.CreateEncryptor();
+                return transform.TransformFinalBlock(input, 0, input.Length);
+            }
+            else
+            {
+                return rijndaelCipher.CreateDecryptor().TransformFinalBlock(input, 0, input.Length);
+            }
+        }
+    }
+}
